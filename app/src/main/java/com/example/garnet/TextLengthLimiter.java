406dc3ca5af97e280a8ba923c121e125dd3e8dfd
9rz <3099936466@qq.com>
2024-04-17 21:36:29 +0800

    SQLite OK

--- app/src/main/java/com/example/garnet/TextLengthLimiter.java
+++ app/src/main/java/com/example/garnet/TextLengthLimiter.java
@@ -36,5 +36,5 @@ public class TextLengthLimiter implements TextWatcher {
     @Override
     public void afterTextChanged(Editable s) {
         tv.setText(s.length() + "/" + MAX_LENGTH);
-    }
+    }//
 }
