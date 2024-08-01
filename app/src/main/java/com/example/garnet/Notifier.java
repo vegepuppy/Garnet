a2e1d041391eb5e1b77fb03dfc9cef81cd18b5f9
9rz <3099936466@qq.com>
2024-08-01 15:02:47 +0800

    feat: 在指定时间时发送一条固定内容通知
    可以实现在硬编码的时间到达时发送通知，并重复通知。
    
    TODO：
    每日一次通知
    通知内容与当日代办相关
    完成待办后取消该事项的通知

---
+++ app/src/main/java/com/example/garnet/Notifier.java
@@ -0,0 +1,29 @@
+package com.example.garnet;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+// 用于创建通知
+public class Notifier extends BroadcastReceiver {
+    public static final String CHANNEL_ID = "channel1";
+    public static final int  NOTIFICATION_ID = 1;
+    public static final String TITLE_EXTRA = "TITLE_EXTRA";
+    public static final String MESSAGE_EXTRA = "MESSAGE_EXTRA";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("今日待办事项提醒")
+                .setContentText("学习高数")
+                .build();
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, notification);
+    }
+}
--- app/src/main/java/com/example/garnet/SettingFragment.java
+++ app/src/main/java/com/example/garnet/SettingFragment.java
@@ -1,64 +1,128 @@
 package com.example.garnet;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
 public class SettingFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingFragment newInstance(String param1, String param2) {
-        SettingFragment fragment = new SettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+        SwitchCompat switchCompat = rootView.findViewById(R.id.daily_notification_switch);
+
+        createNotificationChannel();
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    turnOnNotification();
+                    Log.d("TAG","turned on notification");
+                }else {
+                    Log.d("TAG","turned off notification");
+//                    turnOffNotification();
+                }
+            }
+        });
+        return rootView;
+    }
+
+    private void createNotificationChannel() {
+        // Create a notification channel for devices running
+        // Android Oreo (API level 26) and above
+        String name = "Notify Channel";
+        String desc = "A Description of the Channel";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        NotificationChannel channel = new NotificationChannel(Notifier.CHANNEL_ID, name, importance);
+        channel.setDescription(desc);
+        // Get the NotificationManager service and create the channel
+        NotificationManager notificationManager = (NotificationManager) requireActivity().getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    private void turnOnNotification() {
+        if (checkNotificationPermissions(requireActivity())){
+            scheduleNotification();
+        }
+    }
+
+    private boolean checkNotificationPermissions(Context context) {
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        boolean isEnabled = notificationManager.areNotificationsEnabled();
+
+        if (!isEnabled) {
+            // Open the app notification settings if notifications are not enabled
+            Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, context.getPackageName());
+            context.startActivity(intent);
+
+            return false;
+        }
+
+        // Permissions are granted
+        return true;
+    }
+
+    private void scheduleNotification() {
+        // Create an intent for the Notification BroadcastReceiver
+        Intent intent = new Intent(requireActivity(), Notifier.class);
+
+        // Extract title and message from user input
+        String title = "待办提示";
+        String message = "做题";
+
+        // Add title and message as extras to the intent
+        intent.putExtra(Notifier.TITLE_EXTRA, title);
+        intent.putExtra(Notifier.MESSAGE_EXTRA, message);
+
+        // Create a PendingIntent for the broadcast
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                requireActivity(),
+                Notifier.NOTIFICATION_ID,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE + PendingIntent.FLAG_UPDATE_CURRENT // kotlin 'or' 转写为|
+        );
+
+        // Get the AlarmManager service
+        AlarmManager alarmManager = (AlarmManager) requireContext().getSystemService(Context.ALARM_SERVICE); // 可能有问题
+
+        // Get the selected time and schedule the notification
+        long time = getTime();
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                time,
+                5000, // 每天重复
+                pendingIntent);
+
+        Log.d("TAG", "done setting notification");
+    }
+
+    //获取当前时间的下一分钟，用于调试
+    private long getTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2024,Calendar.AUGUST,1,8,0);
+        return calendar.getTimeInMillis();
     }
 }
