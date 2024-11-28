0c8604c643c43e52143ce1b2db62b9903118aa55
9rz <3099936466@qq.com>
2024-11-28 12:13:26 +0800

    feat: 实现网页端登录

--- GarnetWeb/client/src/App.jsx
+++ GarnetWeb/client/src/App.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import "./App.css";
+import Login from "./components";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-
-      console.log(data);
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{data ? data: "Loading..."}</p>
-      </header>
+      <Login/>
     </div>
   );
 }
 
 export default App;
+
--- GarnetWeb/server/index.js
+++ GarnetWeb/server/index.js
@@ -5,60 +5,77 @@ const PORT = process.env.PORT || 3001;
 const users = [];
 
 const app = express();
+const cors = require('cors')
+app.use(cors({ origin: "http://localhost:5173" }));//允许前端通信
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-
-app.post('/register', (req, res) => {
-    const {username, password} = req.body;
-
-    const userExists = users.some(user => user.username === username);
-    if (userExists){
-      return res.status(400).json({success: false, message:'Username already exists.'})
-    }
-
-    users.push({username, password});
-
-    console.log("registered users:", users);
-
-    res.status(200).json({success:true, message:"user registered successfully."})
+  res.json({ message: "Hello from server!" });
 });
 
-app.post('/login', (req, res) => {
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const userExists = users.some((user) => user.username === username);
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username already exists." });
+  }
+
+  users.push({ username, password });
+
+  console.log("registered users:", users);
+
+  res
+    .status(200)
+    .json({ success: true, message: "user registered successfully." });
+});
+
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   // 验证用户是否存在并且密码正确
-  const user = users.find(user => user.username === username && user.password === password);
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
   if (!user) {
-    console.log(`input username: ${username}, password:${password}, login failed!`)
-      return res.status(401).json({ success: false, message: 'Invalid username or password' });
+    console.log(
+      `input username: ${username}, password:${password}, login failed!`
+    );
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid username or password" });
   }
 
-  res.status(200).json({ success: true, message: 'Login successful' });
+  res.status(200).json({ success: true, message: "Login successful" });
 
-
-  console.log(`username: ${username}, password:${password} login successfully`)
+  console.log(`username: ${username}, password:${password} login successfully`);
 });
 
-app.post('/infoitem', (req, res) => {
+app.post("/infoitem", (req, res) => {
   const dataList = req.body;
 
-    if (!Array.isArray(dataList)) {
-        return res.status(400).json({ success: false, message: 'Invalid data format' });
-    }
+  if (!Array.isArray(dataList)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid data format" });
+  }
 
-    console.log('Received multiple data:', dataList);
+  console.log("Received multiple data:", dataList);
 
-    // 遍历数组并处理每个数据
-    dataList.forEach(data => {
-        const { id, content, belong, display } = data;
-        console.log(`Processing data: id=${id}, content=${content}, belong=${belong}, display=${display}`);
-    });
+  // 遍历数组并处理每个数据
+  dataList.forEach((data) => {
+    const { id, content, belong, display } = data;
+    console.log(
+      `Processing data: id=${id}, content=${content}, belong=${belong}, display=${display}`
+    );
+  });
 
-    res.status(200).json({ success: true, message: 'Data uploaded successfully' });
-})
+  res
+    .status(200)
+    .json({ success: true, message: "Data uploaded successfully" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
