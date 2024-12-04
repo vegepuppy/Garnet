0bc974c19c17f1dfa09eb7ed851241937e0f2213
vegepuppy <371296486@qq.com>
2024-12-04 13:40:05 +0800

    Todo界面完成

--- GarnetWeb/client/src/components/Dashboard/index.jsx
+++ GarnetWeb/client/src/components/Dashboard/index.jsx
@@ -1,12 +1,12 @@
 import {useState} from "react";
-import InfoItemDisplay from "../InfoGroupDisplay";
+import InfoGroupDisplay from "../InfoGroupDisplay";
 import TodoItemDisplay from "../TodoItemsDisplay";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css"
 
 export default function Dashboard() {
 
-    const [activeSection, setActiveSection] = useState("InfoItemDisplay");
+    const [activeSection, setActiveSection] = useState("InfoGroupDisplay");
   return (
     <div>
         <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
@@ -14,15 +14,15 @@ export default function Dashboard() {
                 <Navbar.Brand href="#home">Garnet</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav.Link className="mx-2" onClick={() => setActiveSection("InfoItemDisplay")}
-                        active={activeSection === 'InfoItemDisplay'}>资料列表</Nav.Link>
+                    <Nav.Link className="mx-2" onClick={() => setActiveSection("InfoGroupDisplay")}
+                        active={activeSection === 'InfoGroupDisplay'}>资料列表</Nav.Link>
                     <Nav.Link className="mx-2" onClick={() => setActiveSection("TodoItemDisplay")}
                         active={activeSection === 'TodoItemDisplay'}>待办事项</Nav.Link>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
         <Container style={{ marginTop: '20px' }}>
-            {activeSection === 'InfoItemDisplay' && <InfoItemDisplay />}
+            {activeSection === 'InfoGroupDisplay' && <InfoGroupDisplay />}
             {activeSection === 'TodoItemDisplay' && <TodoItemDisplay />}
         </Container>
     </div>
